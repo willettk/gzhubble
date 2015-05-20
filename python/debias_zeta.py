da5fa86fa1cd77c457f89d2d8d08ff1f8607725c
Kyle Willett <willettk@gmail.com>
2015-05-19 20:24:43 -0500

    Refactored to make plotting its own module

--- python/debias_zeta.py
+++ python/debias_zeta.py
@@ -3,7 +3,11 @@ from scipy import optimize
 from matplotlib import pyplot as plt
 import numpy as np
 
-data = fits.getdata("../ferengi_data.fits",1)
+def get_data():
+    data = fits.getdata("../ferengi_data.fits",1)
+    unique_galaxies = set(data['sdss_id'])
+
+    return data,unique_galaxies
 
 def fzeta_exp(p,x):
     
@@ -38,35 +42,45 @@ def common_labels(fig,xlabel=None,ylabel=None,xfontsize=12,yfontsize=12):
 
     return cax
 
-unique_galaxies = set(data['sdss_id'])
-p_guess = np.array([0.5,0.3,1.])
+def plot_examples(data,unique_galaxies):
 
-# Set up plot
-fig,axarr = plt.subplots(nrows=2,ncols=2,sharex=True,sharey=True)
-bigax = common_labels(fig,'Redshift',r'$f_{features}$',14,20)
-zarr = np.linspace(0,1,50)
-
-for ax in axarr.ravel():
+    p_guess = np.array([0.5,0.3,1.])
+    # Set up plot
+    fig,axarr = plt.subplots(nrows=2,ncols=2,sharex=True,sharey=True)
+    bigax = common_labels(fig,'Redshift',r'$f_{features}$',14,20)
+    zarr = np.linspace(0,1,50)
     
-    slen = 0
-    # Make sure there are enough points in the data to fit a function
-    while slen < (len(p_guess)+1):
-        ind = (data['sdss_id'] == unique_galaxies.pop())
-        slen = sum(ind)
+    for ax in axarr.ravel():
         
-    galaxy1 = data[ind & (data['sim_evolution'] == 0)]
+        slen = 0
+        # Make sure there are enough points in the data to fit a function
+        while slen < (len(p_guess)+1):
+            ind = (data['sdss_id'] == unique_galaxies.pop())
+            slen = sum(ind)
+            
+        galaxy1 = data[ind & (data['sim_evolution'] == 0)]
+    
+        z_gal = galaxy1['sim_redshift']
+        f_gal = galaxy1['p_features']
+    
+        # Data must be explicitly cast as double-type precision for optimization to work. Incredibly frustrating.
+        # Fix: http://stackoverflow.com/questions/12473406/scipy-optimize-leastsq-returns-best-guess-parameters-not-new-best-fit 
+    
+        p, cov, infodict, mesg, ier = optimize.leastsq(errfunc,p_guess,args=(z_gal.astype(np.float64),f_gal.astype(np.float64)),full_output=1)
+        ax.plot(z_gal,f_gal,lw=2)
+        ax.plot(zarr,fzeta_exp(p,zarr),'--',lw=1)
+        ax.set_xlim(0,1)
+        ax.set_ylim(0,1)
+    
+    
+    plt.show()
 
-    z_gal = galaxy1['sim_redshift']
-    f_gal = galaxy1['t00_smooth_or_features_a1_features_frac_weighted_2']
+    return None
 
-    # Data must be explicitly cast as double-type precision for optimization to work. Incredibly frustrating.
-    # Fix: http://stackoverflow.com/questions/12473406/scipy-optimize-leastsq-returns-best-guess-parameters-not-new-best-fit 
-
-    p, cov, infodict, mesg, ier = optimize.leastsq(errfunc,p_guess,args=(z_gal.astype(np.float64),f_gal.astype(np.float64)),full_output=1)
-    ax.plot(z_gal,f_gal,lw=2)
-    ax.plot(zarr,fzeta_exp(p,zarr),'--',lw=1)
-    ax.set_xlim(0,1)
-    ax.set_ylim(0,1)
+def plot_zeta_sb(data):
 
 
-plt.show()
+if __name__ == "__main__":
+
+    data,unique_galaxies = get_data()
+    plot_examples(data,unique_galaxies)
