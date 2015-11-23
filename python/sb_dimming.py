9c32802ed049cc69c4be56b7d265f145efc58ebb
Kyle Willett <willettk@gmail.com>
2015-11-23 16:23:04 -0500

    Plot surface brightness dimming with evolutionary corrections

--- python/sb_dimming.py
+++ python/sb_dimming.py
@@ -18,11 +18,11 @@ data = fits.getdata('%s/ferengi_all_weighted_and_meta.fits' % path,1)
 
 objids = set(data['objid'])
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
+# This is _really_ slow - takes several minutes to run. Not sure
+# why, although maybe something to do with truth selection of FITS rows?
 
-def plot_gals(ax,objid,label=None):
-    gals = (data['objid'] == objid) & (data['sim_evolution'] == 0.)
+def plot_gals(ax,objid,evol=0.0,label=None):
+    gals = (data['objid'] == objid) & (data['sim_evolution'] == evol)
     if gals.sum() == 8:
         # Normalize everything to start at the same surface brightess
         mu0 = data[gals]['mu_max_i'][0]
@@ -31,30 +31,34 @@ def plot_gals(ax,objid,label=None):
             label = label,
             alpha=min(1.,0.3 + (22. - mu0)/4. * 0.7))
 
-
-for objid in objids:
-    plot_gals(ax,objid)
-
-plot_gals(ax,objids.pop(),label='FERENGI')
-
-
-
-# Overplot the expected level of cosmological dimming from Tolman
-
 def cosmo_dim(mu_0,z,z0=0.):
 
     return mu_0 + 10. * np.log10(1. + (z - z0))
 
-
 zarr = np.linspace(0.3,1.,100)
 
-ax.plot(zarr,cosmo_dim(0.0,zarr,z0=0.3),color='red',lw=3,label=r'$(1+z)^4$')
+# Plot
 
-ax.set_xlabel('redshift',fontsize=16)
-ax.set_ylabel(r'$\Delta\mu_{max,i}$',fontsize=20)
+fig = plt.figure()
 
-ax.legend(loc='best')
+for i in range(7):
 
-ax.set_xlim(0,1)
+    ax = fig.add_subplot(3,3,i+1)
+    e = i*0.5
+    
+    for objid in objids:
+        plot_gals(ax,objid,evol=e)
+    
+    plot_gals(ax,next(iter(objids)),label='FERENGI, e={0:.1f}'.format(e))
+
+    # Overplot the expected level of cosmological dimming from Tolman
+    
+    ax.plot(zarr,cosmo_dim(0.0,zarr,z0=0.3),color='red',lw=3,label=r'$(1+z)^4$')
+    
+    ax.set_xlabel('redshift',fontsize=16)
+    ax.set_ylabel(r'$\Delta\mu_{max,i}$',fontsize=20)
+    ax.set_xlim(0,1)
+    ax.set_ylim(-3,4)
+    ax.legend(loc='lower left',fontsize=10)
 
 plt.show()
